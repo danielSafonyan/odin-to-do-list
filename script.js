85dd8396a441e2d295563ae9e91a9739685fc466
danielSafonyan <danielsafonyan@gmail.com>
2022-09-13 14:54:33 +0100

    Implemented Local Storage

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-const cache = {};
+let cache = JSON.parse(localStorage.getItem('cache'));
+console.log(cache);
+if (!cache) {
+    cache = {}
+}
+
+console.log(JSON.stringify(cache));
+
+window.addEventListener('click', () => {
+    localStorage.setItem('cache', JSON.stringify(cache));
+})
 
 function generateID(string) {
     return string.toLowerCase().trim().replaceAll(' ','-');
@@ -27,34 +37,47 @@ class Task {
     }
 }
 
-const taskObjectPrototype = {
-    title: "Get a high-paying job",
-    id: 1,
-    isDone: false
-}
+let allTasksProject =  new Project('All Tasks');
+let currentProject = allTasksProject;
+cache[allTasksProject.id] = allTasksProject;
 
-let spanishProject = new Project('Spanish');
-let spTask1 = new Task("Learn subjuntivo", 'spanish');
-let spTask2 = new Task("Learn vocabulary", 'spanish');
-let spTask3 = new Task("Speak like a native", 'spanish');
+// const taskObjectPrototype = {
+//     title: "Get a high-paying job",
+//     id: 1,
+//     isDone: false
+// }
 
-spanishProject.taskList[spTask1.id] = spTask1;
-spanishProject.taskList[spTask2.id] = spTask2;
-spanishProject.taskList[spTask3.id] = spTask3;
+// let allTasksProject = new Project('All Tasks');
+// let allTask1 = new Task("Get  Groceries", 'all-tasks');
+// // let allTask2 = new Task("Get Lunch", 'all-tasks');
+// // let allTask3 = new Task("Play Voley", 'all-tasks');
 
-let programmingProject = new Project('Programming');
-let prTask1 = new Task("Finish a track", 'programming');
-prTask1.isDone = true;
-let prTask2 = new Task("Create a portofolio", 'programming');
-let prTask3 = new Task("Get a job", 'programming');
+// allTasksProject.taskList[allTask1.id] = allTask1;
+// allTasksProject.taskList[allTask2.id] = allTask2;
+// allTasksProject.taskList[allTask3.id] = allTask3;
 
-programmingProject.taskList[prTask1.id] = prTask1;
-programmingProject.taskList[prTask2.id] = prTask2;
-programmingProject.taskList[prTask3.id] = prTask3;
+// let spanishProject = new Project('Spanish');
+// let spTask1 = new Task("Learn subjuntivo", 'spanish');
+// let spTask2 = new Task("Learn vocabulary", 'spanish');
+// let spTask3 = new Task("Speak like a native", 'spanish');
 
-let currentProject = spanishProject;
-cache[spanishProject.id] = spanishProject;
-cache[programmingProject.id] = programmingProject;
+// spanishProject.taskList[spTask1.id] = spTask1;
+// spanishProject.taskList[spTask2.id] = spTask2;
+// spanishProject.taskList[spTask3.id] = spTask3;
+
+// let programmingProject = new Project('Programming');
+// let prTask1 = new Task("Finish a track", 'programming');
+// prTask1.isDone = true;
+// let prTask2 = new Task("Create a portofolio", 'programming');
+// let prTask3 = new Task("Get a job", 'programming');
+
+// programmingProject.taskList[prTask1.id] = prTask1;
+// programmingProject.taskList[prTask2.id] = prTask2;
+// programmingProject.taskList[prTask3.id] = prTask3;
+
+// cache[spanishProject.id] = spanishProject;
+// cache[programmingProject.id] = programmingProject;
+// cache[allTasksProject.id] = allTasksProject;
 
 const newTaskForm = document.querySelector('#create-task');
 newTaskForm.addEventListener('submit', createNewTask);
@@ -63,13 +86,19 @@ function createNewTask(event) {
     event.preventDefault();
     const taskInputField = event.target[0];
     const taskTitle = taskInputField.value;
+    console.log(taskTitle);
     // create a task object
     const taskObject = new Task(taskTitle, currentProject.id);
+    console.log(taskObject);
     // render a task
     renderTask(taskObject);
     // save a task into the project
     saveTask(taskObject);
-    countRemainigTasks()
+    if (currentProject.id === 'all-tasks') {
+        gatherAllTasks();
+    } else {
+        countRemainigTasks();
+    }
     taskInputField.value = '';
 }
 
@@ -80,6 +109,7 @@ function renderTask(taskObject) {
         const input = document.createElement('input');
         input.setAttribute('type', 'checkbox');
         input.setAttribute('id', taskObject.id);
+        input.setAttribute('project-id', taskObject.projectID);
         input.checked = taskObject.isDone;
         div.appendChild(input);
 
@@ -117,8 +147,14 @@ function changeIsDoneStatus(event) {
     const checkBox = event.currentTarget.querySelector('input');
     checkBox.checked = checkBox.checked ?  false : true;
     const currentTask = event.currentTarget.querySelector('input').getAttribute('id');
-    currentProject.taskList[currentTask].isDone = checkBox.checked;
-    countRemainigTasks()
+    let changedCurrentProject = event.currentTarget.querySelector('input').getAttribute('project-id');
+    cache[changedCurrentProject].taskList[currentTask].isDone = checkBox.checked;
+    console.log(currentProject.id);
+    if (currentProject.id === 'all-tasks') {
+        gatherAllTasks();
+    } else {
+        countRemainigTasks();
+    }
 }
 
 const clearTasksButton = document.querySelector('#clear-tasks');
@@ -131,6 +167,21 @@ function clearAllTasks() {
 }
 
 function clearCompletedTasks() {
+    if (currentProject.id === 'all-tasks') {
+        let allTasks = [];
+        for (const project of Object.values(cache)) {
+            const projectTasks = Object.values(project.taskList)
+            allTasks = allTasks.concat(projectTasks);
+        }
+
+        allTasks.forEach(task => {
+            if (task.isDone) {
+                delete cache[task.projectID].taskList[task.id];
+                document.querySelector(`#${task.id}`).parentElement.remove();
+            }
+        })
+        return;
+    }
     const allTasks = Object.values(currentProject.taskList);
     allTasks.forEach(task => {
         if (task.isDone) {
@@ -186,10 +237,15 @@ function changeCurrentProject(event) {
     const newProjectDisplayed = document.querySelector(`#${event.currentTarget.id}`);
     newProjectDisplayed.classList.add('active-project');
     currentProject = cache[event.currentTarget.id];
-    renderProjectTasks();
+    if (event.currentTarget.id === "all-tasks") {
+        gatherAllTasks();
+        return;
+    } else {
+        renderProjectTasks();
+    }   
 }
 
-const allProjectsTest = document.querySelectorAll('.projects .project');
+const allProjectsTest = document.querySelectorAll('.project');
 allProjectsTest.forEach( project => project.addEventListener('click', changeCurrentProject));
 
 function printCache() {
@@ -199,6 +255,9 @@ function printCache() {
 function renderAllProjects() {
     const allProjects = Object.values(cache);
     for (const [idx, project] of allProjects.entries()) {
+        if (project.id === "all-tasks") {
+            continue;
+        }
         renderProject(project);
         if (idx == 0) {
             document.querySelector(`#${project.id}`).classList.add('active-project');
@@ -207,9 +266,16 @@ function renderAllProjects() {
 }
 
 renderAllProjects();
+renderProjectTasks();
+gatherAllTasks();
 
-function countRemainigTasks() {
-    const currentTaskList = Object.values(currentProject.taskList);
+function countRemainigTasks(taskList) {
+    let currentTaskList = Object.values(currentProject.taskList);
+    if (taskList) {
+        currentTaskList = taskList;
+    } else {
+        currentTaskList = Object.values(currentProject.taskList);
+    }
     const finishedTasks = currentTaskList.filter(task => task.isDone == true);
     const tasksRemaining = currentTaskList.length - finishedTasks.length;
     document.querySelector('#remaining-tasks').innerText = tasksRemaining;
@@ -219,6 +285,9 @@ const deleteProjectButton = document.querySelector('#delete-project');
 deleteProjectButton.addEventListener('click', deleteProject);
 
 function deleteProject(event) {
+    if (currentProject.id === "all-tasks") {
+        return;
+    }
     delete cache[currentProject.id];
     document.querySelector(`#${currentProject.id}`).remove();
     currentProject = cache[Object.keys(cache)[0]];
@@ -238,5 +307,8 @@ function gatherAllTasks() {
         const projectTasks = Object.values(project.taskList)
         allTasks = allTasks.concat(projectTasks);
     }
-    console.log(allTasks);
+    clearAllTasks();
+    allTasks.forEach(task => renderTask(task));
+    document.querySelector('.list-title').innerText = currentProject.title;
+    countRemainigTasks(allTasks);
 }
