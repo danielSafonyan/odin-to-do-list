7f0aee62d95d239e6cfe2ef0e11096d5ce2aa5b6
danielSafonyan <danielsafonyan@gmail.com>
2022-09-19 17:59:30 +0100

    Learned about Observer Patter

---
+++ CurrentProjectObserver.js
@@ -0,0 +1,43 @@
+class CurrentProjectObserver {
+    constructor() {
+        if (CurrentProjectObserver.instance == null) {
+            this._currentProjectID = 'all-tasks';
+            this.subscribers = [];
+            CurrentProjectObserver.instance = this;
+        }
+        return CurrentProjectObserver.instance;
+    }
+
+    subscribe(observer) {
+        this.subscribers.push(observer);
+    }
+
+    unsubscribe(observer) {
+        const observerIndex = this.subscribers.indexOf(observer);
+        if (observerIndex > -1) {
+            this.subscribers.splice();
+        }  else {
+            console.log(`${observer} is not listening.`);
+        }
+    }
+
+    notifyAll() {
+        this.subscribers.forEach(
+            () => console.log("Something changed."),
+        )
+    }
+
+    set currentProjectID(projectID) {
+        this._currentProjectID = projectID;
+        this.notifyAll();
+    }
+
+    get currentProjectID() {
+        return this._currentProjectID;
+    }
+}
+
+
+const currentProject = new CurrentProjectObserver(); 
+Object.seal(currentProject);
+// export default currentProject;
