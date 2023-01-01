d1c69f91b37b88343a09d4026fff0b97a4c8b054
danielSafonyan <danielsafonyan@gmail.com>
2023-01-01 22:11:31 +0000

    Finished transition to React

--- react-to-do-list/src/App.jsx
+++ react-to-do-list/src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Projects from './assets/Projects'
 import Todos from './assets/Todos'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(() => JSON.parse(localStorage.getItem('cache')) || {'all-tasks' : {}})
+  useEffect(function() {
+    localStorage.setItem('cache', JSON.stringify(data))
+  }, [data])
+  const [currentProject, setCurrentProject] = useState('all-tasks')
 
+  function getAllTasks() {
+	const allTasks = []
+	Object.values(data).forEach(el => {
+		allTasks.push(Object.values(el))
+	})
+	return allTasks.flat()
+  }
+  const projectList = currentProject === 'all-tasks' ? getAllTasks() : Object.values(data[currentProject])
   return (
   <div id='container'>
-    <Projects />
-    <Todos />
+    <Projects 
+        projectList={Object.keys(data)}
+        currentProject={currentProject}
+        setCurrentProject={setCurrentProject}
+        setData={setData}
+    />
+    <Todos 
+        taskList={projectList}
+        currentProject={currentProject}
+		setCurrentProject={setCurrentProject}
+		setData={setData}
+    />
   </div>
   )
 }
--- react-to-do-list/src/assets/Projects.jsx
+++ react-to-do-list/src/assets/Projects.jsx
@@ -1,27 +1,74 @@
 import React from 'react'
 
-function Projects() { 
+function Projects(props) { 
+    const [newProjectInput, setNewProjectInput] = React.useState('')
+
+    const projectListElems = props.projectList.map(el => {
+        if (!(el === 'all-tasks')) {
+            return <li 
+                    key={el}
+                    className={`project ${props.currentProject === el ? "active-project" : ''}`} data-project-name={el}
+                    onClick={() => handleProjectClick(el)}
+                    >{el}</li>
+        }
+    })
+
+    function handleProjectClick(project) {
+        props.setCurrentProject(project)
+    }
+
+    function handleFormSubmit(event) {
+        event.preventDefault()
+        const newProjectName = newProjectInput.trim()
+        props.setCurrentProject(newProjectName)
+        props.setData(prev => {
+            if (newProjectName in prev) {
+                return prev
+            }
+            const updatedData = {
+                ...prev,
+                [newProjectName] : {}
+            }
+            return updatedData
+        })
+        setNewProjectInput("")
+    }
+
+    function handleInputChange(event) {
+        setNewProjectInput(event.target.value)
+    }
+
     return (
-         <div className="all-tasks">
+         <div className="all-tasks"> 
             <div className="inboxes">
                 <ul className="project-list">
-                    <li className="project active-project" id="all-tasks">All Tasks</li>
+                    <li 
+                    className={`project ${props.currentProject === 'all-tasks' ? "active-project" : ''}`}
+                    data-project-name="all-tasks"
+                    onClick={() => handleProjectClick('all-tasks')}
+                    >All Tasks</li>
                 </ul>
             </div>
             <div className="projects">
                 <h2 className="project-list-title">My Projects</h2>
             <ul className="dynamic project-list">
-                <li className="project" id="programming">Programming</li>
-                <li className="project active-project" id="spanish">Spanish</li>
-                <li className="project" id="health-&-fitness">Health & Fitness</li> 
+                {projectListElems}
             </ul>
-            <form action="" id="create-project" autocomplete="off">
+            <form 
+                autoComplete="off"
+                onSubmit={handleFormSubmit}
+            >
                 <input 
-                type="text"
-                className="new list"
-                placeholder="new project name"
-                aria-label="new project name"
-                required name="project"/>
+                    type="text"
+                    className="new list"
+                    placeholder="new project name"
+                    aria-label="new project name"
+                    required 
+                    name="project"
+                    maxLength="20"
+                    value={newProjectInput}
+                    onChange={handleInputChange}
+                />
                 <button  className="btn create" aria-label="create new project" type="button">+</button>
             </form>
             </div>
