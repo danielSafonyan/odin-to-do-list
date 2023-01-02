092bba29c8bfca776a9de18634508e7cfffb0dd9
danielSafonyan <danielsafonyan@gmail.com>
2023-01-02 11:32:47 +0000

    Fixed Bugs

--- react-to-do-list/src/App.jsx
+++ react-to-do-list/src/App.jsx
@@ -4,34 +4,35 @@ import Todos from './assets/Todos'
 
 function App() {
   const [data, setData] = useState(() => JSON.parse(localStorage.getItem('cache')) || {'all-tasks' : {}})
-  useEffect(function() {
-    localStorage.setItem('cache', JSON.stringify(data))
-  }, [data])
   const [currentProject, setCurrentProject] = useState('all-tasks')
 
+  useEffect(() => localStorage.setItem('cache', JSON.stringify(data)), [data])
+  
   function getAllTasks() {
-	const allTasks = []
-	Object.values(data).forEach(el => {
-		allTasks.push(Object.values(el))
-	})
-	return allTasks.flat()
+      const allTasks = []
+      Object.values(data).forEach(el => {
+        allTasks.push(Object.values(el))
+      })
+      return allTasks.flat()
   }
-  const projectList = currentProject === 'all-tasks' ? getAllTasks() : Object.values(data[currentProject])
+
+  const taskList = currentProject === 'all-tasks' ? getAllTasks() : Object.values(data[currentProject])
+
   return (
-  <div id='container'>
-    <Projects 
-        projectList={Object.keys(data)}
-        currentProject={currentProject}
-        setCurrentProject={setCurrentProject}
-        setData={setData}
-    />
-    <Todos 
-        taskList={projectList}
-        currentProject={currentProject}
-		setCurrentProject={setCurrentProject}
-		setData={setData}
-    />
-  </div>
+          <div id='container'>
+            <Projects 
+                projectList={Object.keys(data)}
+                currentProject={currentProject}
+                setCurrentProject={setCurrentProject}
+                setData={setData}
+            />
+            <Todos 
+                taskList={taskList}
+                currentProject={currentProject}
+            setCurrentProject={setCurrentProject}
+            setData={setData}
+            />
+          </div>
   )
 }
 
