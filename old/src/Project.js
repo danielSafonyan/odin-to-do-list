08c5bf1149d8252cb119695fd4955575d9b090b4
danielSafonyan <danielsafonyan@gmail.com>
2023-01-02 11:37:22 +0000

    Cleaned up the repo

--- old/src/Project.js
+++
@@ -1,9 +0,0 @@
-import generateID from './generateID.js';
-
-export default class Project {
-  constructor(projectTitle) {
-    this.title = projectTitle;
-    this.id = generateID(projectTitle);
-    this.taskList = {};
-  }
-}
--- old/src/generateID.js
+++
@@ -1,3 +0,0 @@
-export default function generateID(string) {
-  return string.toLowerCase().trim().replaceAll(' ', '-');
-}
